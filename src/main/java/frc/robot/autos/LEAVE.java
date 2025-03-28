7772ef747bb8665d41e68f0810dfbe287afe8ca9
PlusUltra7413 <47487597+PlusUltra7413@users.noreply.github.com>
2025-03-27 17:48:49 -0700

    Add files via upload

---
+++ src/main/java/frc/robot/autos/LEAVE.java
@@ -0,0 +1,21 @@
+/*
+
+package frc.robot.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class LEAVE extends SequentialCommandGroup {
+    
+    DriveSubsystem m_DriveSubsystem;
+
+    public LEAVE(DriveSubsystem m_DriveSubsystem) {
+        this.m_DriveSubsystem = m_DriveSubsystem;
+        
+
+        addCommands(
+        
+        );
+    }
+}
+*/
